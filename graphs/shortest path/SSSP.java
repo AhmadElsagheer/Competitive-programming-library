66df9fc74dd576f5e5d05c9b136fbb97d1914fb5
Ahmad Elsagheer <ahmad.elsagheer@yahoo.com>
2016-06-22 22:31:18 +0200

    added comments to SSSP

--- graphs/shortest path/SSSP.java
+++ graphs/shortest path/SSSP.java
@@ -4,37 +4,42 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
+/*
+ * Single-Source Shortest Path
+ */
 public class SSSP {
 
 	static final int INF = (int)1e9;	//don't increase, avoid overflow
 	static ArrayList<Edge>[] adjList;
 	static int V;
 	
+	/*
+	 * 1. Dijkstra's Algorithm for SSSP on weighted graphs
+	 */
 	static int dijkstra(int S, int T)	//O(E log E)
 	{
 		int[] dist = new int[V];
 		Arrays.fill(dist, INF);
 		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
 		dist[S] = 0;
-		pq.add(new Edge(S, 0));
+		pq.add(new Edge(S, 0));						//may add more in case of MSSP (Mult-Source)
 		while(!pq.isEmpty())
 		{
 			Edge cur = pq.remove();
-			if(cur.node == T)		//remove if all nodes are sinks
+			if(cur.node == T)						//remove if all computations are needed
 				return dist[T];
-			if(cur.cost > dist[cur.node])
+			if(cur.cost > dist[cur.node])			//lazy deletion
 				continue;
 			for(Edge nxt: adjList[cur.node])
 				if(cur.cost + nxt.cost < dist[nxt.node])
-				{
-					dist[nxt.node] = cur.cost + nxt.cost;
-					pq.add(new Edge(nxt.node, dist[nxt.node]));
-				}
-				
+					pq.add(new Edge(nxt.node, dist[nxt.node] = cur.cost + nxt.cost ));
 		}
 		return -1;
 	}
 	
+	/*
+	 * 2. Bellman-Ford's Algorithms for SSSP on weighted graphs with negative cycles
+	 */
 	static boolean bellmanFord(int S)		
 	{
 		int[] dist = new int[V];
@@ -53,29 +58,20 @@ public class SSSP {
 					}
 		}
 		
-		boolean negative_cycle_exists = false;
+		boolean hasNegCycle = false;
 		for(int u = 0; u < V; ++u)
 			for(Edge nxt: adjList[u])
 				if(dist[u] + nxt.cost < dist[nxt.node])
-					negative_cycle_exists = true;
-		return negative_cycle_exists;
+					hasNegCycle = true;
+		return hasNegCycle;
 	}
+	
 	static class Edge implements Comparable<Edge>
 	{
 		int node, cost;
 		
-		Edge(int a, int b)
-		{
-			node = a;
-			cost = b;
-		}
+		Edge(int a, int b) { node = a;	cost = b; }
 		
-		public int compareTo(Edge e)
-		{
-			if(cost != e.cost)
-				return cost - e.cost;
-			return node - e.node;
-		}
-		
+		public int compareTo(Edge e){ return cost - e.cost;	}
 	}
 }
