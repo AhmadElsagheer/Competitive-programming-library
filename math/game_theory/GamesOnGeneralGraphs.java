00c7f3ecad160d656e91d22bad2d44301ef62f35
Ahmad Elsagheer <ahmad.elsagheer@yahoo.com>
2016-07-29 13:59:07 +0200

    added algorithm for games on general graphs

---
+++ math/game_theory/GamesOnGeneralGraphs.java
@@ -0,0 +1,45 @@
+package math.game_theory;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Algorithm for finding winning, losing and draw positions in games
+ * that can represented by a directed graph (possibly with cycles)
+ */
+public class GamesOnGeneralGraphs {
+
+	static boolean[] win, lose, vis;
+	static int[] deg;							//out degree for all nodes in original graph
+	static ArrayList<Integer>[] adjList;		//reverse of original graph
+	static int V;
+	
+	static void go()	// O(V + E)
+	{
+		vis = new boolean[V];
+		win = new boolean[V];
+		lose = new boolean[V];
+		Queue<Integer> q = new LinkedList<Integer>();
+		/*
+		 * Initially insert in the queue all nodes known to be winning or losing
+		 * and mark them either in win or lose.
+		 */
+		while(!q.isEmpty())
+		{
+			int v = q.remove();
+			for(int u: adjList[v])
+			{
+				if(lose[v])
+					win[u] = true;
+				else if(--deg[u] == 0)
+					lose[u] = true;
+				else
+					continue;
+				q.add(u);
+			}
+		}
+		
+		// !win[u] & !lose[u] -> draw[u]
+	}
+}
